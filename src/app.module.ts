d012bac877318abd1c36e3ef23bc21fb3dad0226
Sandro <blacksatiuz@gmx.de>
2023-02-09 21:05:21 +0100

    fix: back to 3000

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { Todo, TodoSchema } from './todo.schema';
 
+process.env.MONGO_URI = 'mongodb://mongo:3l0yqLvZENwv6OMDSXtk@containers-us-west-198.railway.app:5514';
 const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/todo'; // TODO Find a way to set process.env.MONGO_URI for deployment (Docker + GitHub)
 
 @Module({
